775b2a100cf3fee1328a634f38cb97b251fd325c
Kiran Sahu <61940769+kiransahu-dev@users.noreply.github.com>
2023-04-09 09:39:07 +0530

    updated

--- src/action.js
+++ src/action.js
@@ -1,7 +1,6 @@
-export function addAppointment = (appointment) => {
-    return {
-        type: 'ADD_APPOINTMENT',
-        payload: appointment,
-    };
+export const addAppointment = (appointment) => {
+  return {
+    type: 'ADD_APPOINTMENT',
+    payload: appointment,
+  };
 };
-  
